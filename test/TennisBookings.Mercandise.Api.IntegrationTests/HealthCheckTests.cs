54786ff36b420e6ef473f1e9a106005c7a4a9ee1
Dipendu Paul <dipendupaul@gmail.com>
2023-11-26 22:03:06 +0530

    first commit

---
+++ test/TennisBookings.Mercandise.Api.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TennisBookings.Merchandise.Api;
+using Xunit;
+
+namespace TennisBookings.Mercandise.Api.IntegrationTests
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+        public HealthCheckTests(WebApplicationFactory<Startup> factory)
+        {
+                _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task HealthCheck_ReturnsOk()
+        {
+            var response = await _httpClient.GetAsync("/healthcheck");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
